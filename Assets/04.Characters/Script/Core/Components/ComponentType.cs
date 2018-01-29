041aeb7c28f0f3b343cafeb5ad2b7f6cb9f1a048
HdmmY <2439846178@qq.com>
2018-01-30 00:23:18 +0800

    Simplify force applier

--- Assets/04.Characters/Script/Core/Components/ComponentType.cs
+++
@@ -1,6 +0,0 @@
-﻿using UnityEngine;
-
-public class ComponentType
-{
-    public static readonly uint RenderBinding = 1;
-}
---
+++ Assets/04.Characters/Script/Utils/ApplyForce.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using Sirenix.OdinInspector;
+using System.Collections.Generic;
+
+public class ApplyForce : MonoBehaviour
+{
+    [Multiline]
+    public string m_description;
+
+    [System.Serializable]
+    public struct ForceApplier
+    {
+        public Vector3 m_force;
+
+        public Rigidbody m_rig;
+
+        public ForceMode m_forceMode;
+    }
+
+    [ListDrawerSettings(Expanded = false)]
+    public List<ForceApplier> m_forceAppliers;
+
+    public int m_forceMutiplier = 1;
+
+    private void FixedUpdate()
+    {
+        foreach (var forceApplier in m_forceAppliers)
+        {
+            if (forceApplier.m_rig != null)
+            {
+                forceApplier.m_rig.AddForce(
+                    forceApplier.m_force * m_forceMutiplier, forceApplier.m_forceMode);
+            }
+        }
+    }
+}
